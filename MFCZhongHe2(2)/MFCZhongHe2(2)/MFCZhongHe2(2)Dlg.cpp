729e8d811a897807dc7d243e30428e298cbcdd0f
panjinlipqy <1428220334@qq.com>
2020-06-28 17:06:59 +0800

    综合实验2（2） 2020.6.28提交

---
+++ MFCZhongHe2(2)/MFCZhongHe2(2)/MFCZhongHe2(2)Dlg.cpp
@@ -0,0 +1,180 @@
+
+// MFCZhongHe2(2)Dlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "MFCZhongHe2(2).h"
+#include "MFCZhongHe2(2)Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+	enum { IDD = IDD_ABOUTBOX };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(CAboutDlg::IDD)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMFCZhongHe22Dlg 对话框
+
+
+
+
+CMFCZhongHe22Dlg::CMFCZhongHe22Dlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CMFCZhongHe22Dlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCZhongHe22Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCZhongHe22Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+// CMFCZhongHe22Dlg 消息处理程序
+
+BOOL CMFCZhongHe22Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CMFCZhongHe22Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CMFCZhongHe22Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect1;
+		GetClientRect(&rect1);
+		int x = (rect1.Width() - cxIcon + 1) / 2;
+		int y = (rect1.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+
+	CString file;
+	file = "C:\\Users\\14282\\Pictures\\Saved Pictures\\龙猫1.png";
+	CImage img;
+	img.Load(file);
+	CDC* pDC = GetDlgItem(IDC_STATIC)->GetDC();
+	int sx, sy, w, h;
+	CRect rect;
+	GetDlgItem(IDC_STATIC)->GetClientRect(&rect);
+
+	float rect_ratio = 1.0*rect.Width() / rect.Height();
+		float img_ratio = 1.0*img.GetWidth() / img.GetHeight();  //获取客户区和图片的宽高比
+
+		if(rect_ratio > img_ratio)
+		{
+			h = rect.Height();
+			w = img_ratio*h;
+			sx = (rect.Width() - w) /2;
+			sy = 0;
+		}
+		else{
+			w = rect.Width();
+			h = w / img_ratio;
+			sx = 0;
+			sy = (rect.Height() - h)/2;
+		}
+		img.Draw(pDC->m_hDC, sx, sy, w, h);
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CMFCZhongHe22Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
---
+++ MFCZhongHe2(2)/MFCZhongHe2(2)/MFCZhongHe2(2)Dlg.h
@@ -0,0 +1,32 @@
+
+// MFCZhongHe2(2)Dlg.h : 头文件
+//
+
+#pragma once
+
+
+// CMFCZhongHe22Dlg 对话框
+class CMFCZhongHe22Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMFCZhongHe22Dlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_MFCZHONGHE22_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
